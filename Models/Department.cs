dd8be9b54d75238cceea4d34d54e53b672107773
Thomas Bush <thomas.bush8@studytafensw.edu.au>
2023-11-22 19:43:39 +1100

    project start

---
+++ Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api_thomas_bush.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+        [System.Text.Json.Serialization.JsonIgnore]
+    public virtual ICollection<Person> People { get; set; } = new List<Person>();
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+using api_thomas_bush.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// After Add services to the container.
+builder.Services.AddDbContext<RoiDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Define CORS policies to allow access to our API
+builder.Services.AddCors(options => {
+
+    // Build a default CORS policy
+    options.AddDefaultPolicy(policy => {
+
+        // Allow ANY origin to access our API
+        policy.AllowAnyOrigin();
+
+        // Allow SPECIFIC origins to access our API
+        //policy.WithOrigins("http://localhost:19006", "https://app.roi.com.au");
+
+        // Allow any HTTP header
+        policy.AllowAnyHeader();
+
+        // Allow any HTTP method
+        policy.AllowAnyMethod();
+
+    });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Enable CORS (default policy)
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
